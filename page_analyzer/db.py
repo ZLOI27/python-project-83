ab608ea1202f7613780fb857d4da188de9e528a6
Kirill Zlobinov <zlobinovk@yandex.ru>
2026-03-16 22:20:37 +0300

    add: RealDictCursor

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -3,13 +3,14 @@ from contextlib import contextmanager
 
 from dotenv import load_dotenv
 from psycopg2.pool import SimpleConnectionPool
+from psycopg2.extras import RealDictCursor
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 pool = SimpleConnectionPool(
     minconn=1,
-    maxconn=8,
+    maxconn=4,
     dsn=DATABASE_URL,
 )
 
@@ -18,7 +19,7 @@ pool = SimpleConnectionPool(
 def get_cursor():
     conn = pool.getconn()
     try:
-        with conn.cursor() as cur:
+        with conn.cursor(cursor_factory=RealDictCursor)) as cur:
             yield cur
         conn.commit()
     except:
