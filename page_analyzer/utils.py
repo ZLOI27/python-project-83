685f294db1997a3d89274eff154ce31b7aec5fd5
Kirill Zlobinov <zlobinovk@yandex.ru>
2026-03-17 22:53:29 +0300

    chore: implement normalizator and validator

--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -1,6 +1,16 @@
-def normalize_url():
-    pass
+from urllib.parse import urlparse
+
+import validators
 
 
-def validate_url():
-    pass
+def normalize_url(url: str):
+    parsed = urlparse(url.strip())
+    scheme = parsed.scheme.lower()
+    hostname = parsed.hostname.lower()
+    return f"{scheme}://{hostname}"
+
+
+def validate_url(url):
+    if validators.url(url) is not True:
+        return False
+    return True
