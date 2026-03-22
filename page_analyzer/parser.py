e60949bbb81ed8c4e4687b50f69e04d3c4936546
Kirill Zlobinov <zlobinovk@yandex.ru>
2026-03-22 18:11:37 +0300

    add: parser.py

---
+++ page_analyzer/parser.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+
+
+def get_content(html: str):
+    data = {}
+    soup = BeautifulSoup(html, 'html.parser')
+
+    data['h1'] = soup.h1.get_text() if soup.h1 else ''
+    data['title'] = soup.title.get_text() if soup.title else ''
+    description = soup.find('meta', name="description")
+    data['description'] = description.get('content', '') if description else ''
+    return data
--- page_analyzer/services.py
+++ page_analyzer/services.py
@@ -1,11 +1,14 @@
 import requests
 
+from page_analyzer.parser import get_content
+
 
 def check_url(url: str):
     try:
-        response = requests.get(url, timeout=0.5)
+        response = requests.get(url, timeout=1)
         response.raise_for_status()
-        response_data = {'status_code': response.status_code}
+        response_data = get_content(response.text)
+        response_data['status_code'] = response.status_code
         return response_data
     except requests.RequestException:
         return None
